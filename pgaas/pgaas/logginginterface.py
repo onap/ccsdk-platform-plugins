43b5cf23a5031f39fd4fdb4fe4d936e5b81d23c2
Hansen, Tony (th1395) <th1395@att.com>
2018-03-29 15:03:41 +0000

    Revert "Revert "add capability to take over ...""
    This reverts commit 2fcc882d69dc4a59846ef2ff6b352e40c3f5cf49.
    
    Resolve comments from Jack
    
    Change-Id: I8e4ff9a04452a3fa5704b948126b508e78e94bdc
    Signed-off-by: Hansen, Tony (th1395) <th1395@att.com>
    Issue-ID: CCSDK-228
    Signed-off-by: Hansen, Tony (th1395) <th1395@att.com>

--- pgaas/pgaas/logginginterface.py
+++ pgaas/pgaas/logginginterface.py
@@ -1,7 +1,7 @@
 # org.onap.ccsdk
 # ============LICENSE_START====================================================
 # =============================================================================
-# Copyright (c) 2017-2018 AT&T Intellectual Property. All rights reserved.
+# Copyright (c) 2018 AT&T Intellectual Property. All rights reserved.
 # =============================================================================
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,18 +16,32 @@
 # limitations under the License.
 # ============LICENSE_END======================================================
 
-from setuptools import setup, find_packages
+from cloudify import ctx
 
-setup(
-  name="pgaas",
-  version="1.1.0",
-  packages=find_packages(),
-  author="AT&T",
-  description=("Cloudify plugin for pgaas/pgaas."),
-  license="http://www.apache.org/licenses/LICENSE-2.0",
-  keywords="",
-  url="https://onap.org",
-  zip_safe=False,
-  install_requires=[
-  ]
-)
+def debug(msg):
+  """
+  Print a debugging message.
+  This is a handy endpoint to add other extended debugging calls.
+  """
+  ctx.logger.debug(msg)
+
+def warn(msg):
+  """
+  Print a warning message.
+  This is a handy endpoint to add other extended warning calls.
+  """
+  ctx.logger.warn(msg)
+
+def error(msg):
+  """
+  Print an error message.
+  This is a handy endpoint to add other extended error calls.
+  """
+  ctx.logger.error(msg)
+
+def info(msg):
+  """
+  Print a info message.
+  This is a handy endpoint to add other extended info calls.
+  """
+  ctx.logger.info(msg)
