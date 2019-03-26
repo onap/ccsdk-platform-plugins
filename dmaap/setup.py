8f4ec0e0b6a32348302cb318f20a7b93f14b3a6e
xufeiliu <xl085b@att.com>
2019-03-26 21:07:37 +0000

    [20190326] change package name from cloudifydmaapplugin to dmaap
    change package name from cloudifydmaapplugin to dmaap to solve build issue
    
    Issue-ID: CCSDK-794
    Change-Id: Ib7eb3154273660c684d3dbb450cf251a241a95e3
    Signed-off-by: xufeiliu <xl085b@att.com>

--- dmaap/setup.py
+++ dmaap/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "cloudifydmaapplugin",
+    name = "dmaap",
     version = "1.3.2",
     packages=find_packages(),
     author = "AT&T",
